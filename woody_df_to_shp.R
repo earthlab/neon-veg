d832c33b3b5c16cffaf101b691d1273b4b7add25
Victoria Scholl <victoriascholl@gugg181-184-dhcp.colorado.edu>
2018-04-06 16:08:59 -0600

    added woody_df_to_shp.R which creates circular polygons for each tree in NEON woody veg data

---
+++ woody_df_to_shp.R
@@ -0,0 +1,69 @@
+woody_df_to_shp = function(df, coord_ref, shrink, num_sides, shp_filename){
+  # Creates a circular polygon geometry for each veg entry (row) 
+  # within the specified data frame of NEON vegetation structures. 
+  #
+  # Args: 
+  #   df: data frame containing NEON veg structure entries that each
+  #         include the columns: maxCanopyDiameter, easting,
+  #         northing, scientificname, taxonid, individualid,
+  #         stemheight.
+  #   coord_ref: Class "CRS" of Coordinate Reference System Arguments.
+  #         Can be obtained from existing R layer with existing
+  #         "coord .ref" field by using layer@crs
+  #   shrink: numeric integer or decimal; factor by which to divide 
+  #         polygon radii to reduce the size of crown polygons. 
+  #   num_sides: integer; number of sides in each circular polygon
+  #   shp_filename: string; output filename to write shapefile 
+  # 
+  # Returns:
+  #   spdfs:
+  #         spatialPolygonsDataFrame containing crown polygons
+  
+  # for each tree 
+  for (v in 1:length(df$easting)){
+    
+    # calculate radius of circular polygon [units of km] 
+    r = round(df$maxcanopydiam[v]) / 2
+    
+    # reduce radius by shrink factor to reduce polygon size 
+    r = signif( (r / as.numeric(shrink)), 2)
+    
+    # create circular polygon coordinates with center location, 
+    # number of sides, units of distance, polygon calculation
+    c <- circle.polygon(df$easting[v], 
+                        df$northing[v], 
+                        r, sides=num_sides, 
+                        by.length=FALSE, 
+                        units="km", 
+                        poly.type = "cartesian")
+    
+    p = Polygon(c)
+    ps = Polygons(list(p),1)
+    sps = SpatialPolygons(list(ps))
+    
+    # set coordinate reference system
+    proj4string(sps) = coord_ref
+    
+    # create data to associate with the polygon coordinates 
+    d = data.frame(sci_name = df$scientificname[v],
+                   sci_id = df$taxonid[v],
+                   ind_id = df$indvidualid[v],
+                   max_diam = df$maxcanopydiam[v],
+                   height = df$stemheight[v])
+    
+    # turn the SpatialPolygons object into a SpatialPolygonsDataFrame
+    spdf_out = SpatialPolygonsDataFrame(sps,d)
+    
+    # check for the first loop iteration
+    if (v > 1){
+      # If it's a later loop iteration, merge polygons together 
+      spdfs<-rbind(spdf_out, spdfs, makeUniqueIDs = TRUE)
+    } else { 
+      # If it's the first loop iteration, assign the SpatialPolygonsDataFrame to a variable
+      spdfs <- spdf_out
+    }
+  }
+  # write polygon(s) to shapefile  
+  writeOGR(spdfs, getwd(),shp_filename, driver="ESRI Shapefile", overwrite_layer = TRUE)
+  return(spdfs)
+}
