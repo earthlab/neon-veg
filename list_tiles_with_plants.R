3cf2da08ac485ad3612feef4c453e38856227ec4
Victoria Scholl <victoriascholl@gugg181-184-dhcp.colorado.edu>
2018-04-06 16:34:58 -0600

    get a list of which tiles contain plants, to download only the necessary RGB tiles

---
+++ list_tiles_with_plants.R
@@ -0,0 +1,18 @@
+# generate a list of 1km x 1km tiles containing field data
+
+# get easting, northing coordinates of all plants
+e <- NA
+n <- NA
+for (i in 1:dim(woody)[1]){
+  easting <- floor(as.numeric(woody$easting[i])/1000)*1000
+  northing <- floor(as.numeric(woody$northing[i])/1000)*1000
+  e[i] <- easting
+  n[i] <- northing
+}
+
+# find unique rows repesenting tiles
+easting_northings <- data.frame(as.character(e),as.character(n))
+colnames(easting_northings) <- c('e','n')
+tiles <- unique(easting_northings[,c('e','n')])
+
+tiles
