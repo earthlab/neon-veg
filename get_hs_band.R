6055ba311bb60426d985d0864652cac91cb70db1
vscholl <victoria.scholl@colorado.edu>
2018-06-12 11:37:45 -0600

    added optional argument to plot logged reflectance

--- get_hs_band.R
+++ get_hs_band.R
@@ -1,9 +1,19 @@
-get_hs_band <- function(refl, wl, proj4, ext){
-  
-  # refl - array with reflectance data for all wavelengths
-  # wl - wavelength(integer [nm]) to select
-  # proj4 - projection string describing CRS for spatial object
-  # ext - extent of the image to assign georeferences coordinates
+get_hs_band <- function(refl, wl, proj4, ext, plt){
+  # extract a HS band with the specified wavelength (wl)
+  # convert band to a raster, assign CRS and geographic extent, 
+  # transpose for proper plotting orientation
+  #
+  # Args: 
+  #   refl - array with reflectance data for all wavelengths
+  #   wl - wavelength(integer [nm]) to select
+  #   proj4 - projection string describing CRS for spatial object
+  #   ext - extent of the image to assign georeferences coordinates
+  #   plt - boolean variable to plot (TRUE) or not plot (FALSE) the band
+  #
+  # Returns:
+  #   refl.t 
+  #     transposed specified band of reflectance data with CRS and extent;
+  #     ready to plot. 
   
   # get index of closest element in the wavelengths list
   wl.idx <- which.min(abs(wavelengths - wl))
@@ -18,17 +28,17 @@ get_hs_band <- function(refl, wl, proj4, ext){
   # assign UTM extent to raster
   extent(refl.ras) <- ext
   
-  print(refl.ras)
-  
   # transpose x and y values for proper orientation in plot 
   refl.t <- t(refl.ras)
   
   # plot 
-  image(log(refl.t), 
-        main= paste("Band",
-                    as.character(wl),
-                    "nm",
-                    sep= " "))
+  if (plt == TRUE){
+    image(log(refl.t), 
+          main= paste("Band",
+                      as.character(wl),
+                      "nm",
+                      sep= " "))
+  }
   
   return(refl.t)
   
